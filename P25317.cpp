c55a2891da8880dabc6a79b5836f81aacde78ed2
Dustin Jones <dustinlandjones@gmail.com>
2018-09-14 18:24:47 -0600

    trying spi

--- P25317.cpp
+++ P25317.cpp
@@ -1,38 +1,104 @@
 #include <iostream>
 #include <unistd.h> // required for usleep
-#include "GPIO.h"
+#include "P25317.h"
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <stdint.h>
+#include <linux/spi/spidev.h>
+
 
 using namespace std;
 using namespace exploringBB;
 
-P25317::P25317(int pin)
+
+//P25317::P25317(int rst_pin, int cs_pin, char *rx, char *tx)
+P25317::P25317(int rst_pin, int cs_pin)
 {
-  reset_pin = new GPIO(pin);
-  dog = 0;
+  rst = new GPIO(rst_pin);
+  cs = new GPIO(cs_pin);
+  speed = 1000000;
+  bits = 8;
+  mode = 3;
+  rx = new char[MAX_BUF_LEN];
+  tx = new char[MAX_BUF_LEN];
+  // this -> tx = tx;
+  if ((fd = open(SPI_PATH, O_RDWR)) < 0)
+    {
+      perror("SPI Error: Can't open device");
+      return;
+    }
+  printf("fd=%d\n", fd);
 }
 
-P25317::init_display(void)
+void P25317::init_display(void)
 {
-  reset_pin->setDirection(OUTPUT);
-  // here we should keep the reset asserted
+  // reset and CS are outputs
+  rst -> setDirection(OUTPUT);
+  cs -> setDirection(OUTPUT);
+  // keep the reset asserted
   enable_display(0);
+  // open and configure the spi handle
 }
 
-P25317::enable_display(int en)
+void P25317::init_spi(void)
+{
+  if (ioctl(fd, SPI_IOC_WR_MODE, &mode) == -1)
+    {
+      perror("SPI: Can't set SPI mode");
+      return;
+    }
+  if (ioctl(fd, SPI_IOC_RD_MODE, &mode) == -1)
+    {
+      perror("SPI: Can't get SPI mode");
+      return;
+    }
+  if (ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits) == -1)
+    {
+      perror("SPI: Can't set bits per word.");
+      return;
+    }
+  if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits) == -1)
+    {
+      perror("SPI: Can't get bits per word");
+      return;
+    }
+  if (ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed) == -1)
+    {
+      perror("SPI: Can't set max speed HZ");
+      return;
+    }
+  if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed) == -1)
+    {
+      perror("SPI: Can't get max speed HZ");
+      return;
+    }
+  // check properties have been set
+  printf("SPI Mode is: %d\n", mode);
+  printf("SPI Bits is: %d\n", bits);
+  printf("SPI Speed is: %d\n", speed);
+}
+
+void P25317::enable_display(int en)
 {
   if (en)
     {
       //deassert reset line
-      reset_pin->setValue(HIGH);
+      rst -> setValue(HIGH);
+      //NOTE test CS pin as well
+      cs -> setValue(HIGH);
     }
   else
     {
       //assert reset line
-      reset_pin->setValue(LOW);
+      rst -> setValue(LOW);
+      //NOTE test CS pin as well
+      cs -> setValue(LOW);
     }
 }
 
-P25317::toggle_10_x(void)
+void P25317::toggle_10_x(void)
 {
   for (int i = 0; i < 10; i++)
     {
@@ -40,19 +106,58 @@ P25317::toggle_10_x(void)
       usleep(500000);
       enable_display(0);
       usleep(500000);
+
+      tx[0] = i;
+      if (spi_transfer(1) == -1)
+	{
+	  perror("Failed to send SPI word");
+	  return;
+	}
     }
+}
+
+int P25317::spi_transfer(int len)
+{
+  // create local buffers for ioctl...
+  // these pointers need to be last thing pushed to stack
+  //char tx_buf[1];
+  char rx_buf[5];
+
+  // create and configure transfer struct
+  //struct spi_ioc_transfer transfer;
+  spi_ioc_transfer transfer;
+  transfer.tx_buf = (unsigned long)tx;
+  transfer.rx_buf = (unsigned long)rx;
+  transfer.len = 1;
+  transfer.speed_hz = 1000000;
+  transfer.bits_per_word = 8;
+  transfer.delay_usecs = 0;
+
+  // send the spi message
+  int status = ioctl(fd, SPI_IOC_MESSAGE(1), &transfer);
+  if (status < 0)
+    {
+      perror("SPI: SPI_IOC_MESSAGE failed");
+      return -1;
+    }
+  return status;
 }
 
 P25317::~P25317()
 {
-  delete reset_pin;
+  delete rst;
+  delete cs;
 }
+
 
 int main()
 {
-  //  GPIO outGPIO(49), inGPIO(115);
-  P25317 my_disp(59);
-
+  // char tx[MAX_BUF_LEN], rx[MAX_BUF_LEN];
+  //  P25317 my_disp(59, 58, rx, tx);
+  P25317 my_disp(59, 58);
+  
+  my_disp.init_display();
+  my_disp.init_spi();
   my_disp.toggle_10_x();
 
   /*
--- P25317.h
+++ P25317.h
@@ -1,18 +1,31 @@
 #ifndef P25317_H_
 #define P25317_H_
-
 #include "GPIO.h"
+
+using namespace exploringBB;
+
+#define SPI_PATH "/dev/spidev1.0"
+#define MAX_BUF_LEN 100
 
 class P25317
 {
  private:
-  GPIO* reset_pin;
-  int dog;
- public:
+  GPIO *rst, *cs;
+  unsigned int fd, speed;
+  unsigned char mode, bits, value, null;
+  char *tx, *rx;
 
-  P25317(int pin); //constructor
+ public:
+  //  P25317(int rst_pin, int cs_pin, char *rx, char *tx); //constructor
+  P25317(int rst_pin, int cs_pin); //constructor
+
   void init_display(void);
+  void init_spi(void);
+  void enable_display(int en);
   void toggle_10_x(void);
+  int spi_transfer(int len);
 
   ~P25317(); //destructor
-}
+};
+
+#endif
