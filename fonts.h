663fe027ed33b608165d042c8618e30cb73023d1
Dustin Jones <dustinlandjones@gmail.com>
2018-09-23 15:58:20 -0600

    whitespace

--- fonts.h
+++ fonts.h
@@ -5,25 +5,25 @@ extern "C" {
 #ifndef FONTS_H
 #define FONTS_H
 
-/* Font data structure */
-typedef struct
-{
-  unsigned char bytes_per_char;
-  unsigned char font_height;
-  unsigned char font_width;
-  unsigned char first_char;
-  unsigned char last_char;
-  unsigned char *font_table;
-  unsigned char *font_width_table;
-} FONT_T;
+  /* Font data structure */
+  typedef struct
+  {
+    unsigned char bytes_per_char;
+    unsigned char font_height;
+    unsigned char font_width;
+    unsigned char first_char;
+    unsigned char last_char;
+    unsigned char *font_table;
+    unsigned char *font_width_table;
+  } FONT_T;
 
-/* special indexes */
-/* #define SYMBOL_QUARTER          "\x81" */
-/* #define SYMBOL_HALF             "\x82" */
-/* #define SYMBOL_THREE_QUARTER    "\x83" */
+  /* special indexes */
+  /* #define SYMBOL_QUARTER          "\x81" */
+  /* #define SYMBOL_HALF             "\x82" */
+  /* #define SYMBOL_THREE_QUARTER    "\x83" */
 
-/* font tables */
-extern const FONT_T font_big; // 16 pix high
+  /* font tables */
+  extern const FONT_T font_big; // 16 pix high
 
 
 #endif /* FONTS_H */
